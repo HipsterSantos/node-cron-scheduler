0dbd2103e069ffc0d1b61d388d9655812b4dc5ff
HipsterSantos <santoscampos269@gmail.com>
2021-09-02 18:25:37 +0100

    fixed inconsisstent

--- scheduler.js
+++ scheduler.js
@@ -5,6 +5,13 @@ const { resolve } = requir('path');
 
 module.exports  = {
     cronBuilder: (config)=>{
-      
+      Object.keys(config).forEach( (key)=>{
+          if(cron.validate(config[key].frequency)){
+            cron.scheduler(config[key].frequency,(message)=>{
+              const handler = require(resolve(config[key].handler));
+              handler()
+            })
+          }
+      })
     }
 }
